431915fe6c0acaff33690f5a22f3f0ca62cef3cb
Shane Drosi <ShaneDrosi@Joshuas-MacBook-Air.local>
2018-04-26 21:10:05 -0700

    photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,51 +2,51 @@
 // =============================================================
 var friends = [
     {
-      name: "One",
-      picture: "one.jpg",
+      name: "Gene",
+      picture: "../photos/hipsterthree.jpg",
       scores: [
           1,
+          4,
+          2,
+          2,
+          3,
+          3,
           1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
+          3,
+          4,
           1  
       ]
     },
     {
-      name: "Two",
-      picture: "two.jpg",
+      name: "Asher",
+      picture: "../photos/hipsterone.jpg",
       scores: [
           2,
-          2,
-          2,
-          2,
-          2,
-          2,
-          2,
-          2,
-          2,
-          2  
+          3,
+          3,
+          1,
+          5,
+          3,
+          1,
+          1,
+          3,
+          4  
       ]
     },
     {
-      name: "Three",
-      picture: "three.jpg",
+      name: "Gulliver",
+      picture: "../photos/hipstertwo.jpg",
       scores: [
           3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3 
+          2,
+          1,
+          4,
+          2,
+          5,
+          2,
+          4,
+          2,
+          4 
       ]
     },
   ];
