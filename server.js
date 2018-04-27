aa8c2b658e45f57abd201f165bb9ba78a55127ea
Shane Drosi <ShaneDrosi@Joshuas-MacBook-Air.local>
2018-04-26 21:23:24 -0700

    updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
